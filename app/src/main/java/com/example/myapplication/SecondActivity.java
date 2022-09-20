34e862cbc2c5ca91161516cc0ad55ed85d7d1c68
hafeezhaikal <hafizhaikal271@gmail.com>
2022-09-20 11:31:23 +0800

    SECOND COMMIT

---
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+
+    public final static String MESSAGE_KEY = "com.example.myapplication.message_key";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MESSAGE_KEY);
+        TextView textView = new TextView(this);
+        textView.setTextSize(45);
+        textView.setText(message);
+        setContentView(textView);
+    }
+}
